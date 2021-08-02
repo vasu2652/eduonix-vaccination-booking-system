c1e6ae8271998d1307dde80bb6097d58ebf04e98
vasudev <palojuvasudev05@gmail.com>
2021-08-02 11:08:58 +0530

    July 31 , Aug 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
-import './App.css';
+import RetailLogin from './features/retail/components/login';
+import RetailIndex from './features/retail/components';
+import RetailSignUp from './features/retail/components/signup';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserActivity } from './features/retail/slice';
+import NavBar from './navbar';
+import { RetailSnackBar } from './features/retail/components/retail-snackbar';
+//Render Props You pass  component as prop to another component
+
+//PrivateRoute -> HOC -> Higher Order Component -> It takes some simple component and adds a new functionality
+
+//Render props -> 
+
+function PrivateRoute(props) {
+  const { children, loggedIn, ...rest } = props;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        loggedIn  ? (children) : (<Redirect to={{
+          pathname: '/login',
+          state: { from: location }
+        }} />)
+      }
+    />
+  )
+}
 
 function App() {
+  const userActivity = useSelector(selectUserActivity);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar/>
+    <Switch>
+      <PrivateRoute exact path="/" loggedIn={userActivity.loggedIn} >
+        <RetailIndex />
+      </PrivateRoute>
+      <Route path="/login">
+        <RetailLogin/>
+      </Route>
+      <Route path="/signup">
+        <RetailSignUp/>
+      </Route>
+    </Switch>
+    <RetailSnackBar/>
+    </>
   );
 }
 
